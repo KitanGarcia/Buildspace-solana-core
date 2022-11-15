a52ad4ff6bf580f353b314cd92bd0c4790f6d925
Kitan Garcia <kitangar@gmail.com>
2022-11-14 19:25:01 -0800

    fetch and deserialize intros from challenge

--- core1/movie-frontend/challenge-student-intros/components/StudentIntroList.tsx
+++ core1/movie-frontend/challenge-student-intros/components/StudentIntroList.tsx
@@ -1,21 +1,39 @@
-import { Card } from './Card'
-import { FC, useEffect, useState } from 'react'
-import { StudentIntro } from '../models/StudentIntro'
+import { Card } from "./Card";
+import { FC, useEffect, useState } from "react";
+import { StudentIntro } from "../models/StudentIntro";
+import * as Web3 from "@solana/web3.js";
 
-const STUDENT_INTRO_PROGRAM_ID = 'HdE95RSVsdb315jfJtaykXhXY478h53X6okDupVfY9yf'
+const STUDENT_INTRO_PROGRAM_ID = "HdE95RSVsdb315jfJtaykXhXY478h53X6okDupVfY9yf";
 
 export const StudentIntroList: FC = () => {
-    const [studentIntros, setStudentIntros] = useState<StudentIntro[]>([])
+  const connection = new Web3.Connection(Web3.clusterApiUrl("devnet"));
+  const [studentIntros, setStudentIntros] = useState<StudentIntro[]>([]);
 
-    useEffect(() => {
-        setStudentIntros(StudentIntro.mocks)
-    }, [])
-    
-    return (
-        <div>
-            {
-                studentIntros.map((studentIntro, i) => <Card key={i} studentIntro={studentIntro} /> )
+  useEffect(() => {
+    connection
+      .getProgramAccounts(new Web3.PublicKey(STUDENT_INTRO_PROGRAM_ID))
+      .then(async (accounts) => {
+        const studentIntros: StudentIntro[] = accounts.reduce(
+          (accum: StudentIntro[], { pubkey, account }) => {
+            const studentIntro = StudentIntro.deserialize(account.data);
+            if (!studentIntro) {
+              return accum;
             }
-        </div>
-    )
-}
+
+            return [...accum, studentIntro];
+          },
+          []
+        );
+        setStudentIntros(studentIntros);
+      });
+  }, []);
+
+  return (
+    <div>
+      {studentIntros.map((studentIntro, i) => (
+        <Card key={i} studentIntro={studentIntro} />
+      ))}
+    </div>
+  );
+};
+
--- core1/movie-frontend/challenge-student-intros/models/StudentIntro.ts
+++ core1/movie-frontend/challenge-student-intros/models/StudentIntro.ts
@@ -21,6 +21,25 @@ export class StudentIntro {
     return buffer.slice(0, this.borshInstructionSchema.getSpan(buffer));
   }
 
+  static borshAccountSchema = borsh.struct([
+    borsh.u8("initialized"),
+    borsh.str("name"),
+    borsh.str("message"),
+  ]);
+
+  static deserialize(buffer?: Buffer): StudentIntro | null {
+    if (!buffer) {
+      return null;
+    }
+    try {
+      const { name, message } = this.borshAccountSchema.decode(buffer);
+      return new StudentIntro(name, message);
+    } catch (error) {
+      console.log("Deserialization error:", error);
+      return null;
+    }
+  }
+
   static mocks: StudentIntro[] = [
     new StudentIntro(
       "Elizabeth Holmes",
@@ -33,4 +52,3 @@ export class StudentIntro {
     new StudentIntro("Terminator", `i'm basically here to protect`),
   ];
 }
-
