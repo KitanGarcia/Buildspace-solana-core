3c705676e3ca63333f67ca9ff7c7db0cce1865d9
Kitan Garcia <kitangar@gmail.com>
2022-11-15 19:38:27 -0800

    initialize nft-minter; set up chakra ui

---
+++ core1/nft-minter/pages/_app.tsx
@@ -0,0 +1,21 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  background: "#1F1F1F",
+  accent: "#833BBE",
+  bodyText: "rgba(255, 255, 255, 0.75)",
+};
+
+const theme = extendTheme({ colors });
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
