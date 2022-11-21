9743302b9f14a7e5ac0915d6386ca2edb9211447
Kitan Garcia <kitangar@gmail.com>
2022-11-20 21:13:55 -0800

    add code to mint an NFT

--- core2/create-nft/src/index.ts
+++ core2/create-nft/src/index.ts
@@ -1,19 +1,84 @@
-import { initializeKeypair } from "./initializeKeypair"
-import * as web3 from "@solana/web3.js"
+import { initializeKeypair } from "./initializeKeypair";
+import * as Web3 from "@solana/web3.js";
+import { Connection, clusterApiUrl, PublicKey } from "@solana/web3.js";
+import {
+  Metaplex,
+  keypairIdentity,
+  bundlrStorage,
+  toMetaplexFile,
+  NftWithToken,
+} from "@metaplex-foundation/js";
+import * as fs from "fs";
+
+const tokenName = "Token Name";
+const description = "Description";
+const symbol = "SYMBOL";
+const sellerFeeBasisPoints = 100;
+const imageFile = "test.png";
+
+const createNft = async (
+  metaplex: Metaplex,
+  uri: string
+): Promise<NftWithToken> => {
+  const { nft } = await metaplex.nfts().create({
+    uri: uri,
+    name: tokenName,
+    sellerFeeBasisPoints: sellerFeeBasisPoints,
+    symbol: symbol,
+  });
+
+  console.log(
+    `Token Mint: https://explorer.solana.com/address/${nft.address.toString()}?cluster=devnet`
+  );
+
+  return nft;
+};
 
 async function main() {
-  const connection = new web3.Connection(web3.clusterApiUrl("devnet"))
-  const user = await initializeKeypair(connection)
+  const connection = new Web3.Connection(Web3.clusterApiUrl("devnet"));
+  const user = await initializeKeypair(connection);
 
-  console.log("PublicKey:", user.publicKey.toBase58())
+  console.log("PublicKey:", user.publicKey.toBase58());
+
+  // Create metaplex instance
+  const metaplex = Metaplex.make(connection)
+    .use(keypairIdentity(user))
+    .use(
+      bundlrStorage({
+        address: "https://devnet.bundlr.network",
+        providerUrl: "https://api.devnet.solana.com",
+        timeout: 60000,
+      })
+    );
+
+  // File to buffer
+  const buffer = fs.readFileSync(`src/${imageFile}`);
+
+  // Buffer to metaplex file
+  const file = toMetaplexFile(buffer, imageFile);
+
+  // Upload image and get image URI
+  const imageUri = await metaplex.storage().upload(file);
+  console.log(`Image URI: ${imageUri}`);
+
+  // Upload metadata and get metadata URI (off-chain metadata)
+  const { uri } = await metaplex.nfts().uploadMetadata({
+    name: tokenName,
+    description: description,
+    image: imageUri,
+  });
+
+  console.log("Metadata uri:", uri);
+
+  await createNft(metaplex, uri);
 }
 
 main()
   .then(() => {
-    console.log("Finished successfully")
-    process.exit(0)
+    console.log("Finished successfully");
+    process.exit(0);
   })
   .catch((error) => {
-    console.log(error)
-    process.exit(1)
-  })
+    console.log(error);
+    process.exit(1);
+  });
