8c8cf8f0aa3186d262ca6606116a688491f9f309
Kitan Garcia <kitangar@gmail.com>
2022-11-21 21:35:29 -0800

    fetch nfts from wallet

--- core2/display-nfts-frontend/src/components/FetchNft.tsx
+++ core2/display-nfts-frontend/src/components/FetchNft.tsx
@@ -1,10 +1,54 @@
-import { useConnection, useWallet } from "@solana/wallet-adapter-react"
-import { Metaplex, walletAdapterIdentity } from "@metaplex-foundation/js"
-import { FC, useEffect, useState } from "react"
-import styles from "../styles/custom.module.css"
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { Metaplex, walletAdapterIdentity } from "@metaplex-foundation/js";
+import { FC, useEffect, useState } from "react";
+import styles from "../styles/custom.module.css";
 
 export const FetchNft: FC = () => {
-  const fetchNfts = async () => {}
+  const [nftData, setNftData] = useState(null);
 
-  return <div></div>
-}
+  const { connection } = useConnection();
+  const wallet = useWallet();
+  const metaplex = Metaplex.make(connection).use(walletAdapterIdentity(wallet));
+
+  const fetchNfts = async () => {
+    if (!wallet.connected) {
+      return;
+    }
+
+    // fetch NFTs for connected wallet
+    const nfts = await metaplex
+      .nfts()
+      .findAllByOwner({ owner: wallet.publicKey })
+      .run();
+
+    // Fetch off chain metadata for each NFT
+    let nftData = [];
+    for (let i = 0; i < nfts.length; i++) {
+      let fetchResult = await fetch(nfts[i].uri);
+      let json = await fetchResult.json();
+      console.log("NFT METADATA:", json);
+      nftData.push(json);
+    }
+
+    setNftData(nftData);
+  };
+
+  useEffect(() => {
+    fetchNfts();
+  }, [wallet]);
+
+  return (
+    <div>
+      {nftData && (
+        <div className={styles.gridNft}>
+          {nftData.map((nft) => (
+            <div>
+              <ul>{nft.name}</ul>
+              <img src={nft.image} />
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
