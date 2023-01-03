6cad418c2c8ca7a100d6b8f49885089709620c1b
Kitan Garcia <kitangar@gmail.com>
2023-01-02 19:16:33 -0800

    add core5 and counter in anchor

---
+++ core5/counter/tests/counter.ts
@@ -0,0 +1,21 @@
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { Counter } from "../target/types/counter";
+
+describe("counter", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.Counter as Program<Counter>;
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Increments the counter", async () => {
+    const tx = await program.methods.increment().rpc();
+    console.log("Your transaction signature", tx);
+  });
+});
